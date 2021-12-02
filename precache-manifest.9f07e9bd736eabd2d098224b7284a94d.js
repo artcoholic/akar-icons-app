3e1825b413833c97aa519d4e37fe2f0d8df60bc5
artcoholic <agwibawa@gmail.com>
2021-12-02 08:32:40 +0000

    Deploying to gh-pages from @ artcoholic/akar-icons-app@081564a1209935269bca4400cb1166d712cfa086 🚀

---
+++ precache-manifest.9f07e9bd736eabd2d098224b7284a94d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4bd1795318ab7258f07a31db8e2bc6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b23b414c523b33dd89b",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0b23b414c523b33dd89b",
+    "url": "/static/js/2.cccdd12c.chunk.js"
+  },
+  {
+    "revision": "80fc87440188f061986037f9227eea2a",
+    "url": "/static/js/2.cccdd12c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88673fbda7bae3868d7",
+    "url": "/static/js/3.a28f7e7b.chunk.js"
+  },
+  {
+    "revision": "b940861a7fe452a24fc5",
+    "url": "/static/js/main.ea399afe.chunk.js"
+  },
+  {
+    "revision": "137e8153318a9b9d9902",
+    "url": "/static/js/runtime-main.009da128.js"
+  }
+]);
